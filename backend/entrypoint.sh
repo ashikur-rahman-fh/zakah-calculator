ededea76600f903d6277175865c3d67de7e72253
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-03-23 13:25:26 -0400

    Update backend port

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Start the application
-exec gunicorn --bind 0.0.0.0:8000 zakahcal.wsgi:application
+exec gunicorn --bind 0.0.0.0:80 zakahcal.wsgi:application
