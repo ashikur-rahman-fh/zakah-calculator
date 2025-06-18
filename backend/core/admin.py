36ecf719ff656c0da497502732174e01f4c81ce8
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-06-18 13:39:05 -0400

    Add zakah models to admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ZakahCalculation, ZakahTransaction
 # Register your models here.
+
+admin.site.register(ZakahCalculation)
+admin.site.register(ZakahTransaction)
